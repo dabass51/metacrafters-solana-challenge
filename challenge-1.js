59bbbbf5f6132069ce954258a6c7bafe225ba765
dabass51 <heiko.pluemer@googlemail.com>
2022-11-14 19:37:31 +0100

    solved challange

--- challenge-1.js
+++ challenge-1.js
@@ -4,33 +4,25 @@ const {
     PublicKey,
     clusterApiUrl,
     Keypair,
+    SystemProgram,
+    Transaction,
+    sendAndConfirmTransaction,
     LAMPORTS_PER_SOL
 } = require("@solana/web3.js");
 
-// Create a new keypair
-const newPair = new Keypair();
-
-// Exact the public and private key from the keypair
-const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
-const privateKey = newPair._keypair.secretKey;
+// get input var and get public key
+const walletAddress = new PublicKey(process.argv[2]);
 
 // Connect to the Devnet
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-console.log("Public Key of the generated keypair", publicKey);
-
 // Get the wallet balance from a given private key
 const getWalletBalance = async () => {
     try {
-        // Connect to the Devnet
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        console.log("Connection object is:", connection);
-
-        // Make a wallet (keypair) from privateKey and get its balance
-        const myWallet = await Keypair.fromSecretKey(privateKey);
         const walletBalance = await connection.getBalance(
-            new PublicKey(newPair.publicKey)
+            walletAddress
         );
+
         console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`);
     } catch (err) {
         console.log(err);
@@ -39,14 +31,9 @@ const getWalletBalance = async () => {
 
 const airDropSol = async () => {
     try {
-        // Connect to the Devnet and make a wallet from privateKey
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const myWallet = await Keypair.fromSecretKey(privateKey);
-
-        // Request airdrop of 2 SOL to the wallet
         console.log("Airdropping some SOL to my wallet!");
         const fromAirDropSignature = await connection.requestAirdrop(
-            new PublicKey(myWallet.publicKey),
+            walletAddress,
             2 * LAMPORTS_PER_SOL
         );
         await connection.confirmTransaction(fromAirDropSignature);
